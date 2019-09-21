94820950e9b245a489a180029dd742fd6c213176
Anonymous <>
2019-09-21 14:35:17 +1000

    added robot.go

---
+++ robot/robot.go
@@ -0,0 +1,44 @@
+package robot
+
+// Robot .
+type Robot struct {
+	Position  Position
+	Direction string
+}
+
+// Position of a robot .
+type Position struct {
+	X int
+	Y int
+}
+
+func direction() []string {
+	return []string{
+		"NORTH", "EAST", "SOUTH", "WEST",
+	}
+}
+
+// Place will put a new toy robot on a table in position X,Y and facing NORTH, SOUTH, EAST or WEST
+func Place() {
+
+}
+
+// Move will move a toy robot one unit forward in the direction it is currently facing
+func (robot *Robot) Move() {
+
+}
+
+// Left will rotate a robot 90 degrees to the left without changing its position
+func (robot *Robot) Left() {
+
+}
+
+// Right will rotate a robot 90 degrees to the right without changing its position
+func (robot *Robot) Right() {
+
+}
+
+// Report will announce the position and direction of a robot
+func (robot *Robot) Report() {
+
+}
