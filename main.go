dee3e304876d42640c85a66999c458c3e87d8d55
Anonymous <>
2019-09-22 11:37:06 +1000

    fixed command bug

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 
 func main() {
 	var aRobot robot.Robot
+	robotCreated := false
 
 	for {
 		fmt.Print("Command: ")
@@ -25,49 +26,55 @@ func main() {
 		}
 
 		command := strings.ToUpper(scanner.Text())
-		cmd := strings.Split(command, " ")
-		cmd = strings.Split(cmd[1], ",")
-
-		x, _ := strconv.Atoi(cmd[0])
-		y, _ := strconv.Atoi(cmd[1])
-
-		var direction robot.Direction
-
-		switch cmd[2] {
-		case "NORTH":
-			direction = robot.North
-		case "EAST":
-			direction = robot.East
-		case "SOUTH":
-			direction = robot.South
-		case "WEST":
-			direction = robot.West
-		}
-
-		table := table.NewTable(5, 5)
 
 		regex := regexp.MustCompile(`PLACE \d,\d,(NORTH|SOUTH|EAST|WEST)`).MatchString(command)
 		if regex {
+			cmd := strings.Split(command, " ")
+			cmd = strings.Split(cmd[1], ",")
+
+			x, _ := strconv.Atoi(cmd[0])
+			y, _ := strconv.Atoi(cmd[1])
+
+			var direction robot.Direction
+
+			switch cmd[2] {
+			case "NORTH":
+				direction = robot.North
+			case "EAST":
+				direction = robot.East
+			case "SOUTH":
+				direction = robot.South
+			case "WEST":
+				direction = robot.West
+			}
+
+			table := table.NewTable(5, 5)
+
 			// make sure our new robot's position is within the table surface
-			if x > table.Dimensions.X || y > table.Dimensions.Y {
+			if x > table.Dimensions.X || y > table.Dimensions.Y || x < 0 || y < 0 {
 				continue
 			}
 
 			aRobot = robot.Place(robot.Position{X: x, Y: y}, direction)
-			aRobot.Report()
-		}
 
-		switch command {
-		case "MOVE":
-			aRobot.Move()
-		case "LEFT":
-			aRobot.Left()
-		case "RIGHT":
-			aRobot.Right()
-		case "REPORT":
-			aRobot.Report()
+			if !robotCreated {
+				robotCreated = true
+			}
 
-			// fmt.Println("Invalid command entered.")
+			aRobot.Report()
+		} else {
+			switch command {
+			case "MOVE":
+				aRobot.Move()
+			case "LEFT":
+				aRobot.Left()
+			case "RIGHT":
+				aRobot.Right()
+			case "REPORT":
+				aRobot.Report()
+
+				// fmt.Println("Invalid command entered.")
+			}
 		}
 	}
 }
