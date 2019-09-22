bc8b958ca0852ad16446e785e3e3424526f2eb2e
Anonymous <>
2019-09-22 18:52:21 +1000

    added robot_test.go

---
+++ robot/robot_test.go
@@ -0,0 +1,23 @@
+package robot_test
+
+import "testing"
+
+func TestPlace(t *testing.T) {
+
+}
+
+func TestMove(t *testing.T) {
+
+}
+
+func TestLeft(t *testing.T) {
+
+}
+
+func TestRight(t *testing.T) {
+
+}
+
+func TestReport(t *testing.T) {
+
+}
