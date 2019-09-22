787581b16dfd7013d16763ca0d130295282c18d7
Anonymous <>
2019-09-23 09:47:42 +1000

    added basic test functions

--- robot/robot_test.go
+++ robot/robot_test.go
@@ -1,23 +1,89 @@
-package robot_test
+package robot
 
-import "testing"
+import (
+	"testing"
+
+	"../table"
+)
 
 func TestPlace(t *testing.T) {
+	var aRobot *Robot
+	table := table.NewTable(5, 5)
+	command := "PLACE 2,3,EAST"
 
+	aRobot = Process(command, aRobot, table)
+
+	if aRobot == nil {
+		t.Error("")
+	}
+
+	if aRobot.Setting() != "2,3,EAST" {
+		t.Error("")
+	}
 }
 
 func TestMove(t *testing.T) {
+	var aRobot *Robot
+	table := table.NewTable(5, 5)
+	commands := []string{"PLACE 2,3,EAST", "MOVE"}
 
+	for _, command := range commands {
+		aRobot = Process(command, aRobot, table)
+	}
+
+	if aRobot == nil {
+		t.Error("")
+	}
+
+	if aRobot.Setting() != "3,3,EAST" {
+		t.Error("")
+	}
 }
 
 func TestLeft(t *testing.T) {
+	var aRobot *Robot
+	table := table.NewTable(5, 5)
+	commands := []string{"PLACE 2,3,EAST", "LEFT"}
 
+	for _, command := range commands {
+		aRobot = Process(command, aRobot, table)
+	}
+
+	if aRobot == nil {
+		t.Error("")
+	}
+
+	if aRobot.Setting() != "2,3,NORTH" {
+		t.Error("")
+	}
 }
 
 func TestRight(t *testing.T) {
+	var aRobot *Robot
+	table := table.NewTable(5, 5)
+	commands := []string{"PLACE 2,3,EAST", "RIGHT"}
+
+	for _, command := range commands {
+		aRobot = Process(command, aRobot, table)
+	}
+
+	if aRobot == nil {
+		t.Error("")
+	}
+
+	if aRobot.Setting() != "2,3,SOUTH" {
+		t.Error("")
+	}
+}
+
+func TestCaseA(t *testing.T) {
 
 }
 
-func TestReport(t *testing.T) {
+func TestCaseB(t *testing.T) {
+
+}
+
+func TestCaseC(t *testing.T) {
 
 }
