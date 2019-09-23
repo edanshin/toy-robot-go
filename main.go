685f173616b3ccd6259228603bbdf2c56100d677
Anonymous <>
2019-09-23 19:52:29 +1000

    added comments to main file

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	var aRobot *robot.Robot
 	table := table.NewTable(5, 5)
 
+	// if an argument for a file is provided, try to read and execute commands from the file
 	if len(os.Args) == 2 {
 		file, err := os.Open(os.Args[1])
 
@@ -29,6 +30,7 @@ func main() {
 			aRobot = robot.Process(command, aRobot, table)
 		}
 	} else if len(os.Args) == 1 {
+		// if no arguments are provided, execute commands entered via standard console input
 		for {
 			fmt.Print("Robot: ")
 
