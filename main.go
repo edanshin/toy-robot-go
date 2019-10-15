15d81bbaee05b9a3a0b8dc72276d60296da9f748
Anonymous <>
2019-10-15 11:38:32 +1100

    moved text file reading to separate package function

--- main.go
+++ main.go
@@ -1,34 +1,15 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-
 	"./processor"
 	"./robot"
 )
 
 func main() {
-	var aRobot *robot.Robot
+	var aRobot *robot.Robot = &robot.Robot{}
 
 	// if an argument for a file is provided, try to read and execute commands from the text file
-	if len(os.Args) == 2 {
-		file, err := os.Open(os.Args[1])
-
-		if err == nil {
-			defer file.Close()
-			scanner := bufio.NewScanner(file)
-
-			for scanner.Scan() {
-				command := strings.ToUpper(scanner.Text())
-				// output each command to terminal
-				fmt.Println(command)
-				aRobot = robot.Process(command, aRobot)
-			}
-		}
-	}
+	processor.ReadText(aRobot)
 
 	// then execute commands entered via standard console input
 	processor.ReadConsole(aRobot)
