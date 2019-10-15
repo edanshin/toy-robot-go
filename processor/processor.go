dd78bd686fdd6ee8965ff933a773c4284a58e8cf
Anonymous <>
2019-10-15 11:38:56 +1100

    added ReadText function

--- processor/processor.go
+++ processor/processor.go
@@ -21,3 +21,23 @@ func ReadConsole(aRobot *robot.Robot) {
 
 	ReadConsole(aRobot)
 }
+
+// ReadText .
+func ReadText(aRobot *robot.Robot) {
+	// if an argument for a file is provided, try to read and execute commands from the text file
+	if len(os.Args) == 2 {
+		file, err := os.Open(os.Args[1])
+
+		if err == nil {
+			defer file.Close()
+			scanner := bufio.NewScanner(file)
+
+			for scanner.Scan() {
+				command := strings.ToUpper(scanner.Text())
+				// output each command to terminal
+				fmt.Println(command)
+				aRobot = robot.Process(command, aRobot)
+			}
+		}
+	}
+}
