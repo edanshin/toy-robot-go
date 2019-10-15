023cce446301e81b1b795d259b3e16b75bb3d35b
Anonymous <>
2019-10-15 14:24:45 +1100

    added Process function

--- processor/processor.go
+++ processor/processor.go
@@ -5,6 +5,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"../robot"
@@ -17,7 +19,7 @@ func ReadConsole(aRobot *robot.Robot) {
 
 	if scanner.Scan() {
 		command := strings.ToUpper(scanner.Text())
-		aRobot = robot.Process(command, aRobot)
+		aRobot = Process(command, aRobot)
 	}
 
 	ReadConsole(aRobot)
@@ -37,8 +39,60 @@ func ReadText(aRobot *robot.Robot) {
 				command := strings.ToUpper(scanner.Text())
 				// output each command to terminal
 				fmt.Println(command)
-				aRobot = robot.Process(command, aRobot)
+				aRobot = Process(command, aRobot)
 			}
 		}
 	}
+}
+
+// Process processes commands given to robot
+func Process(command string, aRobot *robot.Robot) *robot.Robot {
+	if strings.HasPrefix(command, "PLACE") {
+		// check if submitted command is a valid PLACE command
+		regex := regexp.MustCompile(`PLACE \d,\d,(NORTH|SOUTH|EAST|WEST)`).MatchString(command)
+		if regex {
+			// get command's parameters
+			cmd := strings.Split(command, " ")
+			cmd = strings.Split(cmd[1], ",")
+
+			// get command's coordinate
+			x, _ := strconv.Atoi(cmd[0])
+			y, _ := strconv.Atoi(cmd[1])
+
+			// extract direction
+			var direction robot.Direction
+
+			switch cmd[2] {
+			case "NORTH":
+				direction = robot.North
+			case "EAST":
+				direction = robot.East
+			case "SOUTH":
+				direction = robot.South
+			case "WEST":
+				direction = robot.West
+			}
+
+			aRobot.Place(robot.Position{X: x, Y: y}, direction)
+		} else {
+			fmt.Println("Invalid PLACE command entered.")
+		}
+	} else if command == "MOVE" && aRobot.Placed {
+		aRobot.Move()
+	} else if command == "LEFT" && aRobot.Placed {
+		aRobot.Left()
+	} else if command == "RIGHT" && aRobot.Placed {
+		aRobot.Right()
+	} else if command == "REPORT" && aRobot.Placed {
+		aRobot.Report()
+		aRobot.Display()
+	} else if command == "EXIT" {
+		os.Exit(0)
+	} else if !aRobot.Placed && command != "EXIT" {
+		fmt.Println("Error: robot is not placed.")
+	} else {
+		fmt.Println("Invalid command entered.")
+	}
+
+	return aRobot
 }
