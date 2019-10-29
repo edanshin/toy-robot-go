9f7c947cdde1469f3adb7bbc8db343d4697578e9
Anonymous <no@mail>
2019-10-30 10:32:45 +1100

    updated processor.go - optimized ReadText function

--- src/processor/processor.go
+++ src/processor/processor.go
@@ -26,23 +26,25 @@ func ReadConsole(aRobot *robot.Robot) {
 }
 
 // ReadText reads and executes commands from a text file
-func ReadText(aRobot *robot.Robot) {
+func ReadText(aRobot *robot.Robot, filepath string) bool {
 	// if an argument for a file is provided, try to read and execute commands from the text file
-	if len(os.Args) == 2 {
-		file, err := os.Open(os.Args[1])
+	file, err := os.Open(filepath)
 
-		if err == nil {
-			defer file.Close()
-			scanner := bufio.NewScanner(file)
-
-			for scanner.Scan() {
-				command := strings.ToUpper(scanner.Text())
-				// output each command to terminal
-				fmt.Println(command)
-				aRobot = Process(command, aRobot)
-			}
-		}
+	if err != nil {
+		return false
 	}
+
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		command := strings.ToUpper(scanner.Text())
+		// output each command to terminal
+		fmt.Println(command)
+		aRobot = Process(command, aRobot)
+	}
+
+	return true
 }
 
 // Process processes commands given to robot
